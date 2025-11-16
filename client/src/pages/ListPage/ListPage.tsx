aef62485554665d29b7df88eb260d51c9ccc13d2
Egor <egor.toropov@yahoo.com>
2025-11-17 02:04:13 +0500

    add(ListPage): добавить фокус на вводе

--- client/src/pages/ListPage/ListPage.tsx
+++ client/src/pages/ListPage/ListPage.tsx
@@ -1,8 +1,10 @@
-import { Button, Text, TextInput } from '@gravity-ui/uikit';
+import { Button, Hotkey, Text, TextInput } from '@gravity-ui/uikit';
 import FilterAside from './FilterAside.tsx';
 import ResultSection from './ResultSection.tsx';
 import { setSearch } from '../../core/store/Slices/filter.ts';
 import { useAppDispatch, useAppSelector } from '../../core/store/store.ts';
+import { useHotkeys } from '../../core/hooks/useHotkeys.ts';
+import { useRef } from 'react';
 
 function ListPage() {
   const dispatch = useAppDispatch();
@@ -16,11 +18,18 @@ function ListPage() {
     }
     dispatch(setSearch(searchText));
   };
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSearchFocus = (e?: KeyboardEvent) => {
+    e?.preventDefault();
+    inputRef.current?.focus();
+  };
+  useHotkeys([{ key: '/', handler: handleSearchFocus }]);
   return (
     <>
       <div
         style={{
-          margin: '24px 0',
+          margin: '14px 0',
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
@@ -33,12 +42,16 @@ function ListPage() {
           size={'l'}
           style={{ fontWeight: '700', backgroundColor: 'white', borderRadius: '10px' }}
           placeholder={'Поиск по объявлениям'}
+          controlRef={inputRef}
           onUpdate={handleSearchChange}
           value={search}
         />
         <Button size={'l'} view={'flat-contrast'} style={{ height: 'max-content' }}>
           Найти
         </Button>
+      </div>
+      <div>
+        <Hotkey value={'/'} /> - Фокус на вводе
       </div>
       <Text variant={'display-1'} style={{ fontWeight: '700', margin: '12px 0' }}>
         Объявления на модерации
